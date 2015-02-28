29deed45d1c830ad207771f5132936979aeafafd
fortesl <fortesl@yahoo.com>
2015-02-28 08:23:52 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+Install
+=======
+bower install lf-angular-toastr
+
+
+USAGE
+=====
+<code>
+var myApp = angular.module('myApp', ['lf-toastr']);<br>
+myApp.controller('MyController', ['lfToastrService', function(lfToastrService) {<br>
+&nbsp;&nbsp;&nbsp;    //call methods here as per API below<br><br>
+});
+</code>
+
+API
+===
+<ol>
+    <li><b>thereAreOpenedToasts</b> returns true if there are toasts, returns false otherwise
+        <ul><li>Example: <code>if (lfToastrService.thereAreOpenedToasts() { ... do something ... }</code></li></ul>
+    </li>
+    <li><b>clearToasts</b> removes all opened toasts
+        <ul><li>Example: <code>lfToastrService.clearToasts();</code></li></ul>
+    </li>
+    <li><b>toastIsOpened</b> returns true if a toast is opened, returns false otherwise
+        <ul><li>Example: <code>if (lfToastrService.toastIsOpened() { ... do something ... }</code></li></ul>
+    </li>
+    <li><b>openToast</b> opens a toast, given a message string, a title string, and an optional config object
+        <ul><li>Example: <code>lfToastrService.openToast('I love toasts', 'TOAST ME', {type: 'success'});</code></li></ul>
+    </li>
+    <li><b>clearToast</b> removes a toast, given a message string
+        <ul><li>Example: <code>lfToastrService.removeToast('I love toasts');</code></li></ul>
+    </li>
+</ol>
+
+
+DOCUMENTATION
+=============
+Find info on angular-toastr here:
+<a href="https://github.com/Foxandxss/angular-toastr">Foxandxss angular-toasts</a>
---
+++ lf-angular-toastr.js
@@ -0,0 +1,68 @@
+/**
+ * Name: lf-angular-toastr.js
+ * Created by lfortes on 2/28/2015.
+ */
+
+(function () {
+    'use strict';
+
+    angular.module('lf-toastr', ['toastr', 'ng-animate']).factory('lfToastrService', ['toastr', function (toastr) {
+
+        var openedToasts = [];
+        var toastDefaultOptions = {
+            positionClass: 'toast-bottom-right',
+            tapToDismiss: false,
+            timeOut: 0,
+            extendedTimeOut: 0,
+            type: 'error'
+        };
+
+        function thereAreOpenedToasts() {
+            return openedToasts.length;
+        }
+
+        function clearToasts() {
+            toastr.clear();
+            openedToasts.length = 0;
+        }
+
+        function toastIsOpened(message) {
+            for (var i = 0; i < openedToasts.length; i++) {
+                if (openedToasts[i].scope.message === message) break;
+            }
+            return (openedToasts.length && i < openedToasts.length);
+        }
+
+        function openToast(message, title, options) {
+            var currentOptions = {};
+            if (!options) {
+                currentOptions = toastDefaultOptions;
+            }
+            else {
+                currentOptions = angular.extend({}, toastDefaultOptions, options);
+            }
+            openedToasts.push(toastr[currentOptions.type](message, title, currentOptions));
+        }
+
+        function clearToast(message) {
+            for (var i = 0; i < openedToasts.length; i++) {
+                if (openedToasts[i].scope.message === message) break;
+            }
+            if (i < openedToasts.length) {
+                var toasts = openedToasts.splice(i, 1);
+                toastr.clear(toasts[0]);
+            }
+        }
+
+        // Service API
+        return {
+            thereAreOpenedToasts: thereAreOpenedToasts,
+            clearToasts: clearToasts,
+            toastIsOpened: toastIsOpened,
+            openToast: openToast,
+            clearToast: clearToast
+        };
+
+    }]);
+
+})();
